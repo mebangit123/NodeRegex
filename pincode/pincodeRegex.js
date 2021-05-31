49467c37090e52f718d7335444e97e38d647fe04
mebangit123 <mebannongrum333@gmail.com>
2021-05-31 21:05:27 +0530

    Merging pincodeUC4 to main branch

--- pincode/pincodeRegex.js
+++ pincode/pincodeRegex.js
@@ -1,5 +1,5 @@
-let pincode = '400088B';
-let pincodeRegex = RegExp('^[1-9]{1}[0-9]{5}$');
+let pincode = '400 088';
+let pincodeRegex = RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$');
 
 if(pincodeRegex.test(pincode))
 	console.log("Valid Pincode");
