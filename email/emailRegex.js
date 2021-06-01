4221587ed1ce5640b92cc94c1408784054221e02
mebangit123 <mebannongrum333@gmail.com>
2021-06-01 15:38:07 +0530

    Merging EmailRegexUC4 to main branch

--- email/emailRegex.js
+++ email/emailRegex.js
@@ -1,5 +1,5 @@
-let email = 'meban@gmail.com';
-let emailRegex = RegExp('[a-zA-Z]+*@[a-zA-Z]+.[a-zA-Z]{2,4}');
+let email = 'meban.111@gmail.com';
+let emailRegex = RegExp('^[0-9a-zA-Z]+([._-+][0-9a-zA-Z])*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}$');
 
 if(emailRegex.test(email))
 	console.log("Valid Email");
