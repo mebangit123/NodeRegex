cce239c726a3d07a834dd790d3fccd23594f4b39
mebangit123 <mebannongrum333@gmail.com>
2021-06-01 15:28:35 +0530

    Merging EmailRegexUC3 to main branch

--- email/emailRegex.js
+++ email/emailRegex.js
@@ -1,5 +1,5 @@
-let email = 'abc';
-let emailRegex = RegExp('^[a-zA-Z]+@[a-zA-Z]+');
+let email = 'meban@gmail.com';
+let emailRegex = RegExp('[a-zA-Z]+*@[a-zA-Z]+.[a-zA-Z]{2,4}');
 
 if(emailRegex.test(email))
 	console.log("Valid Email");
